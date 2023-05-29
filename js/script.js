ba224433517eea30eea78674937a3304eb2b2245
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-05-29 09:24:52 +0300

    Добавлен функционал отправки запроса на сервер и вывод результата на экран. Фикс двойного нажатия на иконку поиска

--- js/script.js
+++ js/script.js
@@ -4,16 +4,33 @@ const label = document.querySelector("[data-label]");
 label.onclick = function () {
   // Находим поисковую строку(инпут) при клике на лейбл
   const search = document.querySelector("[data-search]");
-  let hidden = search.getAttribute("hidden");
 
   // При наличии атрибута hidden убираем его, при отсутствии добавляем
-  if (hidden) {
-    search.removeAttribute("hidden");
-  } else {
-    search.setAttribute("hidden", "hidden");
-  }
+  search.toggleAttribute("hidden");
 
-  // !!!!!!! Клик отрабатывает со второго раза
+  // Вешаем прослушку на инпут
+  search.addEventListener("input", function () {
+    let inputValue = this.value;
+
+    //  Находим div, в который разместим результат поиска
+    const searchLinks = document.querySelector(".header__search-link");
+
+    // Отправляем запрос на сервер при вводе пяти символов
+    if (inputValue.length === 5) {
+      const requestData = fetch("https://jsonplaceholder.typicode.com/photos?albumId=1&_limit=5").then((response) => response.json());
+
+      requestData.then((json) =>
+        json.map((item) => {
+          const requestItemHTML = `<a href="${item.url}" target="_blank">link on your request №${item.id}</a>`;
+          searchLinks.insertAdjacentHTML("beforeend", requestItemHTML);
+        })
+      );
+    } else if (inputValue.length > 5) {
+      // Очищаем инпут и результат поиска при вводе более пяти символов
+      searchLinks.innerHTML = " ";
+      this.value = "";
+    }
+  });
 };
 
 /*================ BURGER-MENU ================*/
